e11d958147cae639ab23418836199010083aeba5
Areum-Jung <areumii95@naver.com>
2021-10-08 13:44:04 +0900

    메인 페이지 slider 섹션 contact-info JS효과 추가

--- js/main.js
+++ js/main.js
@@ -48,32 +48,101 @@ $(document).ready(function(){
 
 });
 
-
+/* 추가 */
 // 공지사항/프로모션 스와이퍼(17, 32강 참고) -----------------------------------------------------------------------------------------------
 // swiper(선택자, 옵션)
-new Swiper('.notice-line .swiper-container', {
-	// 객체 데이터
-	direction: 'vertical',
-	autoplay: true,           // 자동재생여부
-	loop: true                // 반복재생여부 (1-2-3-4-1-2-3-4-1 ...)
-  });
+// new Swiper('.notice-line .swiper-container', {
+// 	// 객체 데이터
+// 	direction: 'vertical',
+// 	autoplay: true,           // 자동재생여부
+// 	loop: true                // 반복재생여부 (1-2-3-4-1-2-3-4-1 ...)
+//   });
   
-  new Swiper('.promotion .swiper-container', {
-	// direction: 'horizontal' => swiper 함수에 기본값으로 들어가있음
-	slidesPerView: 3,        // 한번에 보여줄 슬라이드 개수
-	spaceBetween: 10,        // 슬라이드 사이 여백
-	centeredSlides: true,    // 1번 슬라이드가 가운데 보이기
-	loop: true,              // 반복재생여부
-	autoplay: {              // 자동재생여부 - 5s
-	  delay: 5000       
-	},
-	pagination: {
-	  el: '.promotion .swiper-pagination',   //페이지 번호 요소 선택자
-	  clickable: true   //사용자의페이지 번호 요소 제어가능 여부 (클릭가능 여부)
-	},
-	navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-	  prevEl: '.promotion .swiper-prev',
-	  nextEl: '.promotion .swiper-next'
-	}
-  });
+//   new Swiper('.promotion .swiper-container', {
+// 	// direction: 'horizontal' => swiper 함수에 기본값으로 들어가있음
+// 	slidesPerView: 3,        // 한번에 보여줄 슬라이드 개수
+// 	spaceBetween: 10,        // 슬라이드 사이 여백
+// 	centeredSlides: true,    // 1번 슬라이드가 가운데 보이기
+// 	loop: true,              // 반복재생여부
+// 	autoplay: {              // 자동재생여부 - 5s
+// 	  delay: 5000       
+// 	},
+// 	pagination: {
+// 	  el: '.promotion .swiper-pagination',   //페이지 번호 요소 선택자
+// 	  clickable: true   //사용자의페이지 번호 요소 제어가능 여부 (클릭가능 여부)
+// 	},
+// 	navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+// 	  prevEl: '.promotion .swiper-prev',
+// 	  nextEl: '.promotion .swiper-next'
+// 	}
+//   });
 
+
+
+/* 추가 */
+// 메인 페이지 컨텍트 아이콘 클릭시 애니메이션 효과 적용
+let emailEls = document.querySelectorAll('.contact')
+let contactEl = document.querySelectorAll('.contact_info')
+
+console.log('emailEl >>> ', emailEls)
+console.log('contactEl >>> ', contactEl)
+
+emailEls.forEach((emailEl, index) => emailEl.addEventListener('click', function(){
+
+	if(emailEl.classList.contains('selected')) {
+		emailEl.classList.remove('selected');
+		contactEl[index].style.display = 'none'
+	} else {
+		emailEl.classList.add('selected')
+		contactEl[index].style.display = 'block'
+	}
+	
+}))
+
+// let emailEl = document.getElementById('email')
+// let contactEl = emailEl.querySelector('.contact_info')
+
+// 시행착오1
+// emaildiv.addEventListener('focus', function() {
+// 	emailEl.classList.add('focused')
+// 	console.log('focus 이벤트 호출')
+// 	emaildiv.innerText = 'j.areum92@gamil.com'
+// })
+
+// emaildiv.addEventListener('blur', function() {
+// 	emailEl.classList.remove ('focused')
+// 	console.log('blur 이벤트 호출')
+// 	emaildiv.innerText = ''
+// })
+
+// 시행착오2
+// function select() {
+	
+// 	if(emailEls.classList.contains('selected')) {
+// 		emailEls.classList.remove('selected');
+// 		contactEl.style.display = 'none'
+// 	} else {
+// 		emailEls.classList.add('selected')
+// 		contactEl.style.display = 'block'
+// 	}
+// }
+
+// 시행착오3
+// emailEl.addEventListener('click', function(){
+	
+// 	if(emailEl.classList.contains('selected')) {
+// 		emailEl.classList.remove('selected');
+// 		contactEl.style.display = 'none'
+// 	} else {
+// 		emailEl.classList.add('selected')
+// 		contactEl.style.display = 'block'
+// 	}
+
+// })
+
+// 시행착오4
+// if(emailEls.classList.contains('cont-email')) {
+	// 	select(index)
+	// } else if(emailEls.classList.contains('cont-phone')) {
+	// 	select(index)
+	// }
